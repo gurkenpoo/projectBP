3a5149d84a80f8de827846ecb039ef928c74aef0
gurkenpoo <bastianpoo@gmail.com>
2024-04-17 13:02:56 -0400

    adding psx shaders and player movment

---
+++ Scripts/testingArea.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+@onready var player = $player
+
+func _physics_process(_delta):
+	print($player.global_position)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+func _unhandled_input(_event):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
